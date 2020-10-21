d08bbb5daa69a4a14558d61dd1bddbb205337f53
Brijesh Kumar RAJAK <BrijeshKumar.RAJAK@amadeus.com>
2020-10-21 16:59:16 +0530

    changed message to hello

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome Azure App Service Demo ! '
+    return 'Hello This is App Service Demo ! '
 
 
 if __name__ == '__main__':
